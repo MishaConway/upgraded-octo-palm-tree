78ae20eb4b94ed18e7abab11e2430bf6bb7b1983
Misha Conway <mishaAconway@gmail.com>
2018-02-03 03:01:58 -0800

    enable mipmapping

--- ogl/textures/texture.cpp
+++ ogl/textures/texture.cpp
@@ -91,11 +91,11 @@ void OpenGL::Texture::Setup( const unsigned int width, const unsigned int height
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
     else{
-       /* glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glGenerateMipmap( GL_TEXTURE_2D ); */
+        glGenerateMipmap( GL_TEXTURE_2D );
     }
     
     //Upload the textel buffer using Pixel Buffer Objects
