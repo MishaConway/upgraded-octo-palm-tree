79cb05b923198c7296ba94efea73843b0f0e1352
Misha Conway <mishaAconway@gmail.com>
2018-03-05 00:10:49 -0800

    updates to toon shader

--- shaders/pixel/includes/toon.h
+++ shaders/pixel/includes/toon.h
@@ -1,16 +1,28 @@
 
-
+float stepmix(float edge0, float edge1, float E, float x)
+{
+    float T = clamp(0.5 * (x - edge0 + E) / E, 0.0, 1.0);
+    return mix(edge0, edge1, T);
+}
 
 
 
 
 float toonify_factor( float factor, float levels ){
-    const float scale_factor = 1.0 / levels;
-    
-    float num_levels = floor(factor / scale_factor);
-    
-    return num_levels * factor;
+    float E = fwidth(factor);
     
     
-    //return floor(diffuse_factor * levels) * scale_factor;
+
+    
+    float amount_per_level = 1.0 / levels;
+    
+    float num_levels = floor(factor / amount_per_level);
+    
+    return num_levels * amount_per_level;
+}
+
+vec3 ramp( float f ){
+    return sample_tex_3( vec2( f, f) ).rgb;
+    
+    
 }
